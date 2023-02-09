1973c52a8cf5a287be56d76f650f40b471b3e775
yang445786754 <tony_9410@foxmail.com>
2023-02-09 19:38:31 +0800

    [fix] fix bug

--- call_function_with_timeout/__init__.py
+++ call_function_with_timeout/__init__.py
@@ -1,6 +1,6 @@
-from .timeout import SetTimeout
+from .timeout import SetTimeout, SetTimeoutDecorator
 
 
 __author__ = "Tony_9410 (tony_9410@foxmail.com)"
-__version__ = "1.1.0"
-__last_modification__ = "2022.07.27"
+__version__ = "1.1.1"
+__last_modification__ = "2023.02.09"
